ba2d242a1c1771c3ce1d1efaf6c40441454b47a8
hailicy <2572729959@qq.com>
2020-04-07 14:24:13 +0800

    '文件上传和删除都写了'

--- javapro/src/main/java/com/taotie/wechatpro/utils/filwUpDownUtil.java
+++ javapro/src/main/java/com/taotie/wechatpro/utils/filwUpDownUtil.java
@@ -3,6 +3,7 @@ package com.taotie.wechatpro.utils;
 import com.google.gson.Gson;
 import com.qiniu.common.QiniuException;
 import com.qiniu.http.Response;
+import com.qiniu.storage.BucketManager;
 import com.qiniu.storage.Configuration;
 import com.qiniu.storage.Region;
 import com.qiniu.storage.UploadManager;
@@ -16,13 +17,11 @@ import java.io.*;
 /**
  * 创建时间: 2020/4/7 11:30
  * 文件备注:
- * 编写人员:
+ * 编写人员: 杨伯益
  */
 
-//ak  BBJfruU970ss5X448tA8Nxk9hu8g49z6_2vjx06F
-    // sk  9WCki9RQwgup0G2cNDKMd73uOfSlmi-2s9XFnQtp
 
-public class imgUpload {
+public class filwUpDownUtil {
 
     //传入一个包含图片或视频的inputStream，返回外链字符串
     public static String upload(FileInputStream fileInputStream) {
@@ -60,10 +59,34 @@ public class imgUpload {
 
 
 
+    //传入文件url，删除成功返回true
+    public static boolean delete(String url){
+        String key=url.replace("http://q8ee90frk.bkt.clouddn.com/","");//url中去除前面的域名http://q8ee90frk.bkt.clouddn.com/之后就是key值
+        //构造一个带指定 Region 对象的配置类
+        Configuration cfg = new Configuration(Region.region0());
+        //...其他参数参考类注释
+        String accessKey = "BBJfruU970ss5X448tA8Nxk9hu8g49z6_2vjx06F";
+        String secretKey = "9WCki9RQwgup0G2cNDKMd73uOfSlmi-2s9XFnQtp";
+        String bucket = "xiaochengxuimg";
+        Auth auth = Auth.create(accessKey, secretKey);
+        BucketManager bucketManager = new BucketManager(auth, cfg);
+        try {
+            bucketManager.delete(bucket, key);
+            return true;
+        } catch (QiniuException ex) {
+            //如果遇到异常，说明删除失败
+            System.err.println(ex.code());
+            System.err.println(ex.response.toString());
+            return false;
+        }
+    }
 
 
 
 
+
+
+//下面为测试用代码
 
 
 
@@ -152,7 +175,28 @@ public class imgUpload {
         }
     }
 
-
+    //url中去除前面的域名http://q8ee90frk.bkt.clouddn.com/之后就是key值
+    @Test
+    public void deleteTest(){
+        String url="http://q8ee90frk.bkt.clouddn.com/FtAV1olX_77RCzi9TAcEKijiHTPK";
+        String key=url.replace("http://q8ee90frk.bkt.clouddn.com/","");//url中去除前面的域名http://q8ee90frk.bkt.clouddn.com/之后就是key值
+        //构造一个带指定 Region 对象的配置类
+        Configuration cfg = new Configuration(Region.region0());
+        //...其他参数参考类注释
+        String accessKey = "BBJfruU970ss5X448tA8Nxk9hu8g49z6_2vjx06F";
+        String secretKey = "9WCki9RQwgup0G2cNDKMd73uOfSlmi-2s9XFnQtp";
+        String bucket = "xiaochengxuimg";
+        Auth auth = Auth.create(accessKey, secretKey);
+        BucketManager bucketManager = new BucketManager(auth, cfg);
+        try {
+            bucketManager.delete(bucket, key);
+            System.out.println("删除成功！");
+        } catch (QiniuException ex) {
+            //如果遇到异常，说明删除失败
+            System.err.println(ex.code());
+            System.err.println(ex.response.toString());
+        }
+    }
 
 
 }
