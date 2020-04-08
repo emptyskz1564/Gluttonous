338ec50667eba193502a49d594e8beca10e58dd8
sakebow <719878756@qq.com>
2020-04-08 21:40:28 +0800

    添加了一点点注释到微信用户页中

--- wechat/pages/user/user.js
+++ wechat/pages/user/user.js
@@ -6,6 +6,7 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 保存用户信息
     userInfo: {}
   },
 
@@ -13,7 +14,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
   },
 
   /**
@@ -25,29 +25,6 @@ Page({
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              app.globalData.userInfo = res.userInfo
-              // 修改data时必须使用setData方法，否则无法保存修改
-              that.setData({
-                userInfo: res.userInfo
-              })
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (app.userInfoReadyCallback) {
-                app.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
       }
     })
   },
